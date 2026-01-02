4de2269b13e2c8753fbd37dfc17bb017a77bb81d
DivyaKhunt07 <info.dkoladiya07@gmail.com>
2026-01-02 21:49:02 +0000

    Project home screen update

--- app/src/main/java/com/example/myapplicationmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationmvvm/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplicationmvvm.ui.theme.MyApplicationMVVMTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Divya",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,7 +35,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = Color.Red,
         modifier = modifier
+
     )
 }
 
